cd744608d88717d85b04473c2375b280d4fdf4ad
QC <qball@gmpclient.org>
2013-02-10 12:10:03 +0100

    Add modules header list.

---
+++ stk-list.hpp
@@ -0,0 +1,25 @@
+#ifndef __STK_LIST_HPP__
+#define __STK_LIST_HPP__
+#include <string>
+#include "stk-module.hpp"
+
+namespace Stuffkeeper
+{
+	// Forward declr.
+	class CLI;
+	
+
+	class List : public Module
+	{
+		public:
+		List(CLI *cli):
+			Module(cli, "list")
+		{
+
+		}
+
+		int run (int argc, char **argv);
+
+	};
+}
+#endif
